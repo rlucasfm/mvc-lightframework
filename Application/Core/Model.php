d632d3e11a0012876166ab9dd6d014e2bd2d59aa
Richard Lucas Furtado de Mendonça <richard@grupovoz.local>
2021-03-19 15:34:31 -0300

    Base Model

---
+++ Application/Core/Model.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Application\Core;
+
+/**
+ * Classe que implementa métodos básicos comuns aos 
+ * modelos que serão utilizados no Framework
+ * 
+ * @author Richard Lucas <richardlucasfm@gmail.com>
+ * @version 0.1
+ */
+class Model
+{
+
+}
